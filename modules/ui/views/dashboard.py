c086f979223971478209aad68a3eb43b09d20a76
david-i-berry <dberry@wmo.int>
2026-03-04 14:56:12 +0100

    Update and split of dashboard into user and admin dashboards.

--- modules/ui/views/dashboard.py
+++ modules/ui/views/dashboard.py
@@ -7,5 +7,5 @@ GRAFANA_URL = os.getenv("WIS2_GRAFANA_URL", "http://localhost:3000")
 def render(container):
     with container:
         ui.element('iframe') \
-            .props(f'src="{GRAFANA_URL}/d/wis2-downloader?kiosk&theme=light"') \
+            .props(f'src="{GRAFANA_URL}/d/wis2-downloader-overview?kiosk&theme=light"') \
             .classes('grafana-frame')
